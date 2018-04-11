f84cc43907df1d1e71bcb1f54249e2ef48b0a34e
James Hamann <jameshamann@me.com>
2018-04-11 14:37:04 +0100

    adds backend services

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 
+Amplify.configure(aws_exports);
+
+let apiName = 'sampleCloudApi';
+let path = '/items';
+API: {
+    endpoints: [
+        {
+            name: "MyCustomLambdaApi",
+            endpoint: "https://lambda.us-east-1.amazonaws.com/2015-03-31/functions/yourFuncName/invocations",
+            service: "lambda",
+            region: "us-east-1"
+        }
+    ]
+}
 class App extends Component {
+
+  componentDidMount(){
+    API.get(apiName, path, myInit).then(response => {
+      console.log(response)
+    });
+  }
+
   render() {
     return (
       <div className="App">
